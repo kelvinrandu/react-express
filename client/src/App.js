1e6d2b5cc1a20a57cc06b35597fd5056c4955c3f
kelvinrandu <randukelvin@gmail.com>
2022-01-10 08:48:02 +0300

    added component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Users from './components/users/users';
+import React,{useState,useEffect} from 'react';
 
 function App() {
+  const [users,setUsers] = useState([])
+  
+    useEffect(() => {
+      getUsers();
+      console.log(users);
+    }, []);
+
+    async function getUsers() {
+      const response = await fetch("/api/users" ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    });
+      const users = await response.json();
+      setUsers(users);
+    }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Users/>
+      {users}
+
     </div>
   );
 }
---
+++ client/src/components/users/users.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function users() {
+    return (
+        <div>
+            <h2> users</h2>
+            
+        </div>
+    )
+}
